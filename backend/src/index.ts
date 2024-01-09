507f238571321f09c22f5cf7eb208fbd53e4fb95
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-09 22:24:59 +0200

    F/AddHotel/Backend/Asterisk get req

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
-import path from 'path'
+import path, { join } from 'path'
 import { v2 as cloudinary } from 'cloudinary'
 
 
@@ -33,6 +33,10 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')))
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/my-hotels', myHotelRoutes)
+
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist.index.html'))
+})
 
 app.listen(7000, () => {
     console.log('server running on localhost:7000')
