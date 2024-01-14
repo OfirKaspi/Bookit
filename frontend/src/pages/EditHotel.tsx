0e89a6f7f94aed645c78f547970a5b67c396ba10
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-14 16:48:29 +0200

    F/EditHotel/Frontend/Succes and err msgs + add hotel bug fixed

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,9 +2,11 @@ import { useMutation, useQuery } from "react-query"
 import { useParams } from "react-router-dom"
 import { fetchMyHotelById, updateMyHotelById } from "../api-client"
 import { ManageHotelForm } from "../forms/ManageHotelForm/ManageHotelForm"
+import { useAppContext } from "../contexts/AppContext"
 
 export const EditHotel = () => {
     const { hotelId } = useParams()
+    const { showToast } = useAppContext()
 
     const { data: hotel } = useQuery('fetchMyHotelById', () => fetchMyHotelById(hotelId || ''), {
         enabled: !!hotelId,
@@ -12,10 +14,10 @@ export const EditHotel = () => {
 
     const { mutate, isLoading } = useMutation(updateMyHotelById, {
         onSuccess: async () => {
-            // showToast({ message: 'Hotel Saved!', type: 'SUCCESS' })
+            showToast({ message: 'Hotel Saved!', type: 'SUCCESS' })
         },
         onError: () => {
-            // showToast({ message: 'Error Saving Hotl', type: 'ERROR' })
+            showToast({ message: 'Error Saving Hotel', type: 'ERROR' })
         }
     })
 
