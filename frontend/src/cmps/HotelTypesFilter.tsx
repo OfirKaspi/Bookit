4704dc47d6f2a2fe77305fbac942b29a412a099f
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-15 20:43:19 +0200

    F/Search/Frontend/Filter by type,facilities and price

---
+++ frontend/src/cmps/HotelTypesFilter.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent } from "react"
+import { hotelTypes } from "../config/hotel-options-config"
+
+type Props = {
+    selectedHotelTypes: string[]
+    onChange: (Event: ChangeEvent<HTMLInputElement>) => void
+}
+
+export const HotelTypesFilter = ({ selectedHotelTypes, onChange }: Props) => {
+    return (
+        <div className="border-b border-slate-300 pb-5">
+            <h4 className="text-md font-semibold mb-2">Hotel Type</h4>
+            {hotelTypes.map((hoteType) => (
+                <label className="flex items-center space-x-2">
+                    <input
+                        type="checkbox"
+                        className="rounded"
+                        value={hoteType}
+                        checked={selectedHotelTypes.includes(hoteType)}
+                        onChange={onChange}
+                    />
+                    <span>{hoteType}</span>
+                </label>
+            ))}
+        </div>
+    )
+}
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -5,11 +5,17 @@ import { ChangeEvent, useState } from "react"
 import { SerachResultsCard } from "../cmps/SerachResultsCard"
 import { Pagination } from "../cmps/Pagination"
 import { StarRatingFilter } from "../cmps/StarRatingFilter"
+import { HotelTypesFilter } from "../cmps/HotelTypesFilter"
+import { FacilitiesFilter } from "../cmps/FacilitiesFilter"
+import { PriceFilter } from "../cmps/PriceFilter"
 
 export const Search = () => {
     const search = useSearchContext()
     const [page, setPage] = useState<number>(1)
     const [selectedStars, setSelectedStars] = useState<string[]>([])
+    const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([])
+    const [selectedFacilities, setSelectedFacilities] = useState<string[]>([])
+    const [selectedPrice, setSelectedPrice] = useState<number | undefined>()
 
     const searchParams = {
         destination: search.destination,
@@ -19,6 +25,9 @@ export const Search = () => {
         childCount: search.childCount.toString(),
         page: page.toString(),
         stars: selectedStars,
+        types: selectedHotelTypes,
+        facilities: selectedFacilities,
+        maxPrice: selectedPrice?.toString(),
     }
 
     const { data: hotelData } = useQuery(['searchHotels', searchParams], () =>
@@ -27,13 +36,29 @@ export const Search = () => {
 
     const handleStarsChange = (event: ChangeEvent<HTMLInputElement>) => {
         const starRating = event.target.value
-
         setSelectedStars((prevStars) =>
             event.target.checked
                 ? [...prevStars, starRating]
                 : prevStars.filter((star) => star !== starRating)
         )
+    }
 
+    const handleHotelTypeChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const hotelType = event.target.value
+        setSelectedHotelTypes((prevHotelTypes) =>
+            event.target.checked
+                ? [...prevHotelTypes, hotelType]
+                : prevHotelTypes.filter((prevHotelType) => prevHotelType !== hotelType)
+        )
+    }
+
+    const handleFacilityChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const facility = event.target.value
+        setSelectedFacilities((prevFavilities) =>
+            event.target.checked
+                ? [...prevFavilities, facility]
+                : prevFavilities.filter((prevFacility) => prevFacility !== facility)
+        )
     }
 
     return (
@@ -44,6 +69,9 @@ export const Search = () => {
                         Filter by:
                     </h3>
                     <StarRatingFilter selectedStars={selectedStars} onChange={handleStarsChange} />
+                    <HotelTypesFilter selectedHotelTypes={selectedHotelTypes} onChange={handleHotelTypeChange} />
+                    <FacilitiesFilter selectedFacilities={selectedFacilities} onChange={handleFacilityChange} />
+                    <PriceFilter selectedPrice={selectedPrice} onChange={(value?: number) => setSelectedPrice(value)} />
                 </div>
             </div>
             <div className="flex flex-col gap-5">
