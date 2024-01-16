6f3aac50c7f299a35b7abeb54529a9cedff85c35
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-16 16:09:29 +0200

    F/HotelDetails/Frontend/Showing images, description and facilities

--- frontend/src/pages/HotelDetails.tsx
+++ frontend/src/pages/HotelDetails.tsx
@@ -6,7 +6,7 @@ import { AiFillStar } from "react-icons/ai"
 export const HotelDetails = () => {
     const { hotelId } = useParams()
     const { data: hotel } = useQuery('fetchHotelById', () =>
-        fetchHotelById(hotelId as string), {
+        fetchHotelById(hotelId || ''), {
         enabled: !!hotelId
     })
 
@@ -21,6 +21,30 @@ export const HotelDetails = () => {
                     ))}
                 </span>
             </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                {hotel.imageUrls.map((image) => (
+                    <div key={image} className="h-[300px]">
+                        <img src={image} alt={hotel.name} className="rounded-md w-full h-full object-cover object-center" />
+                    </div>
+                ))}
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
+                {hotel.facilities.map((facility) => (
+                    <div className="border border-slate-300 rounded-sm p-3">
+                        {facility}
+                    </div>
+                ))}
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-[2fr-1fr]">
+                <div className="whitespace-pre-line">{hotel.description}</div>
+                <div className="h-fit">
+                    {/* <GeustInfo/> */}
+                </div>
+            </div>
+
         </div>
     )
 }
