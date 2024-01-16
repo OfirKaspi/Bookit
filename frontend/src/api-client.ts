dfc395916b0f4a162e9d73fe437eb6016024c20b
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-16 15:57:57 +0200

    F/HotelDetails/Frontrnd/Get hotel by id

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -129,3 +129,9 @@ export const searchHotels = async (searchParams: SearchParams): Promise<HotelSea
     if (!response.ok) throw new Error('Failed to update Hotel')
     return response.json()
 }
+
+export const fetchHotelById = async (hotelId: string) => {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`)
+    if (!response.ok) throw new Error('Error fetching Hotels')
+    return response.json()
+}
