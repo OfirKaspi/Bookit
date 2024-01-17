ad6e8b0ba073fd9f64f58eb3f55ad3b2bb1daff4
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-17 16:06:09 +0200

    F/Booking/Frontend/First form skeleton (name,email)

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query"
 import { fetchCurrentUser } from "../api-client"
+import { BookingForm } from "../forms/BookingForm/BookingForm"
 
 export const Booking = () => {
     const { data: currentUser } = useQuery(
@@ -8,6 +9,9 @@ export const Booking = () => {
     )
 
     return (
-        <div></div>
+        <div className="grid md:grid-cols-[1fr_2fr]">
+            <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
+            {currentUser && <BookingForm currentUser={currentUser} />}
+        </div>
     )
 }
