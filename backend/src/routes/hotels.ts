480e5cae6885792ba72e069ecfe37fb9b2e41c6b
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-17 18:35:32 +0200

    F/Booking/Backend/Payment endpoint using stripe + new booking type + schema

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
 import Hotel from '../models/hotel'
-import { HotelSearchResponse } from '../shared/types'
+import { BookingType, HotelSearchResponse } from '../shared/types'
 import { param, validationResult } from 'express-validator'
 import Stripe from 'stripe'
 import { verifyToken } from '../middleware/auth'
@@ -97,6 +97,41 @@ router.post('/:hotelId/booking/payment-intent', verifyToken, async (req: Request
 
     res.send(response)
 
+})
+
+router.post('/:hotelId/bookings', verifyToken, async (req: Request, res: Response) => {
+    try {
+        const paymentIntentId = req.body.paymentIntentId
+        const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId as string)
+
+        if (!paymentIntent) return res.status(400).json({ message: 'Payment intent not found' })
+
+        if (paymentIntent.metadata.hotelId !== req.params.hotelId ||
+            paymentIntent.metadata.userId !== req.userId
+        ) {
+            return res.status(400).json({ message: 'Payment intent mismatch' })
+        }
+
+        if (paymentIntent.status !== 'succeeded') {
+            return res.status(400).json({ message: `Payment intent not succeeded. status ${paymentIntent.status}` })
+        }
+
+        const newBooking: BookingType = { ...req.body, userId: req.userId }
+
+        const hotel = await Hotel.findOneAndUpdate(
+            { _id: req.params.hotelId },
+            { $push: { bookings: newBooking } }
+        )
+
+        if (!hotel) return res.status(400).json({ message: 'Hotel not found' })
+
+        await hotel.save()
+        res.status(200).send()
+
+    } catch (err) {
+        console.log('error', err)
+        res.status(500).send({ message: 'Something went wrong' })
+    }
 })
 
 const constructSearchQuery = (queryParams: any) => {
