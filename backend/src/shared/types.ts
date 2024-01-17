ec29793a42512d7b8653202ab07693bd59ac0fcd
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-17 16:56:20 +0200

    F/Booking/Backend/Payment endpoint using stripe

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -7,27 +7,33 @@ export type UserType = {
 }
 
 export type HotelType = {
-    _id: string,
-    userId: string,
-    name: string,
-    city: string,
-    country: string,
-    description: string,
-    type: string,
-    adultCount: number,
-    childCount: number,
-    facilities: string[],
-    pricePerNight: number,
-    starRating: number,
-    imageUrls: string[],
-    lastUpdated: Date,
+    _id: string
+    userId: string
+    name: string
+    city: string
+    country: string
+    description: string
+    type: string
+    adultCount: number
+    childCount: number
+    facilities: string[]
+    pricePerNight: number
+    starRating: number
+    imageUrls: string[]
+    lastUpdated: Date
 }
 
 export type HotelSearchResponse = {
-    data: HotelType[],
+    data: HotelType[]
     pagination: {
-        total: number,
-        page: number,
+        total: number
+        page: number
         pages: number
     }
+}
+
+export type PaymentIntentResponse = {
+    paymentIntentId: string
+    clientSecret: string
+    totalCost: number
 }
