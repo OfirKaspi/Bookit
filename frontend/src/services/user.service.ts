7bbd79d320a12771c0f57159e77b029b945a8a17
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-21 13:46:37 +0200

    F/Convention/Separate the api-client into 2 main services, next step is to work with them and delete the api-client file

---
+++ frontend/src/services/user.service.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { RegisterFormData } from "../pages/Register";
+import { UserType } from "../../../backend/src/shared/types";
+import { SignInFormData } from "../pages/SignIn";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
+
+export const fetchCurrentUser = async (): Promise<UserType> => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/api/users/me`, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching user');
+    }
+};
+
+export const register = async (formData: RegisterFormData) => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/api/users/register`, formData, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        throw new Error('User already exists');
+    }
+};
+
+export const signIn = async (formData: SignInFormData) => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/api/auth/login`, formData, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        throw new Error('Invalid credentials');
+    }
+};
+
+export const validateToken = async () => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/api/auth/validate-token`, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        throw new Error('Token invalid');
+    }
+};
+
+export const signOut = async () => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/api/auth/logout`, null, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        throw new Error('Error during signout');
+    }
+};
