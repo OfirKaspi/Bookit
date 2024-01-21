96de4fef1fab4928635337565d25530cb73d54f6
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-21 15:17:18 +0200

    F/Navigate to home page when user sign out

--- frontend/src/cmps/SignOutButton.tsx
+++ frontend/src/cmps/SignOutButton.tsx
@@ -1,16 +1,19 @@
 import { useMutation, useQueryClient } from "react-query"
 import { useAppContext } from "../contexts/AppContext"
 import { signOut } from "../services/user.service"
+import { useNavigate } from "react-router"
 
 export const SignOutButton = () => {
 
     const queryClient = useQueryClient()
     const { showToast } = useAppContext()
+    const navigate = useNavigate()
 
     const mutation = useMutation(signOut, {
         onSuccess: async () => {
             await queryClient.invalidateQueries('validateToken')
             showToast({ message: 'Sign Out!', type: 'SUCCESS' })
+            navigate('/')
         },
         onError: (error: Error) => {
             showToast({ message: error.message, type: 'ERROR' })
