f0eaac3fc59ed64a3d75c2b3d968abb67364bc5d
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-21 14:17:39 +0200

    F/Convention/Add credeniatls for every req

--- frontend/src/services/hotel.service.ts
+++ frontend/src/services/hotel.service.ts
@@ -1,122 +1,124 @@
 import { HotelSearchResponse, HotelType, PaymentIntentResponse } from "../../../backend/src/shared/types"
-import { BookingFormData } from "../forms/BookingForm/BookingForm";
-import Axios from "axios";
+import { BookingFormData } from "../forms/BookingForm/BookingForm"
+import Axios from "axios"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
+const axios = Axios.create({ withCredentials: true })
+
 type SearchParams = {
-    destination?: string;
-    checkIn?: string;
-    checkOut?: string;
-    adultCount?: string;
-    childCount?: string;
-    page?: string;
-    facilities?: string[];
-    types?: string[];
-    stars?: string[];
-    maxPrice?: string;
-    sortOption?: string;
-};
+    destination?: string
+    checkIn?: string
+    checkOut?: string
+    adultCount?: string
+    childCount?: string
+    page?: string
+    facilities?: string[]
+    types?: string[]
+    stars?: string[]
+    maxPrice?: string
+    sortOption?: string
+}
 
 export const addMyHotel = async (hotelFormData: FormData) => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/my-hotels`, hotelFormData, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/my-hotels`, hotelFormData)
+        return response.data
     } catch (error) {
-        throw new Error('Fail to add hotel');
+        throw new Error('Fail to add hotel')
     }
-};
+}
 
 export const fetchMyHotels = async (): Promise<HotelType[]> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/my-hotels`, { withCredentials: true });
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/my-hotels`)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching hotels');
+        throw new Error('Error fetching hotels')
     }
-};
+}
 
 export const fetchMyHotelById = async (hotelId: string): Promise<HotelType> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/my-hotels/${hotelId}`, { withCredentials: true });
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/my-hotels/${hotelId}`)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching hotel');
+        throw new Error('Error fetching hotel')
     }
-};
+}
 
 export const updateMyHotelById = async (hotelFormData: FormData) => {
     try {
-        const response = await Axios.put(`${API_BASE_URL}/api/my-hotels/${hotelFormData.get('hotelId')}`, hotelFormData, { withCredentials: true });
-        return response.data;
+        const response = await axios.put(`${API_BASE_URL}/api/my-hotels/${hotelFormData.get('hotelId')}`, hotelFormData,)
+        return response.data
     } catch (error) {
-        throw new Error('Failed to update hotel');
+        throw new Error('Failed to update hotel')
     }
-};
+}
 
 export const searchHotels = async (searchParams: SearchParams): Promise<HotelSearchResponse> => {
     try {
-        const queryParams = new URLSearchParams();
+        const queryParams = new URLSearchParams()
 
         // Manually append each parameter to the URLSearchParams
         Object.entries(searchParams).forEach(([key, value]) => {
             if (value !== undefined) {
                 if (Array.isArray(value)) {
-                    value.forEach((v) => queryParams.append(key, v));
+                    value.forEach((v) => queryParams.append(key, v))
                 } else {
-                    queryParams.append(key, value);
+                    queryParams.append(key, value)
                 }
             }
-        });
+        })
 
-        const response = await Axios.get(`${API_BASE_URL}/api/hotels/search?${queryParams.toString()}`);
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/hotels/search?${queryParams.toString()}`)
+        return response.data
     } catch (error) {
-        throw new Error('Failed to update hotel');
+        throw new Error('Failed to update hotel')
     }
-};
+}
 
 export const fetchHotels = async (): Promise<HotelType[]> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/hotels`);
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/hotels`)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching hotels');
+        throw new Error('Error fetching hotels')
     }
-};
+}
 
 export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/hotels/${hotelId}`);
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/hotels/${hotelId}`)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching hotel');
+        throw new Error('Error fetching hotel')
     }
-};
+}
 
 export const createPaymentIntent = async (hotelId: string, numberOfNights: string): Promise<PaymentIntentResponse> => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/hotels/${hotelId}/bookings/payment-intent`, { numberOfNights }, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/hotels/${hotelId}/bookings/payment-intent`, { numberOfNights },)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching payment intent');
+        throw new Error('Error fetching payment intent')
     }
-};
+}
 
 export const createRoomBooking = async (formData: BookingFormData) => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`, formData, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`, formData)
+        return response.data
     } catch (error) {
-        throw new Error('Error booking room');
+        throw new Error('Error booking room')
     }
-};
+}
 
 export const fetchMyBookings = async (): Promise<HotelType[]> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/my-bookings`, { withCredentials: true });
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/my-bookings`)
+        return response.data
     } catch (error) {
-        throw new Error('Unable to fetch bookings');
+        throw new Error('Unable to fetch bookings')
     }
-};
+}
--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,51 +1,53 @@
-import Axios from "axios";
-import { RegisterFormData } from "../pages/Register";
-import { UserType } from "../../../backend/src/shared/types";
-import { SignInFormData } from "../pages/SignIn";
+import Axios from "axios"
+import { RegisterFormData } from "../pages/Register"
+import { UserType } from "../../../backend/src/shared/types"
+import { SignInFormData } from "../pages/SignIn"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
+const axios = Axios.create({ withCredentials: true })
+
 export const fetchCurrentUser = async (): Promise<UserType> => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/users/me`, { withCredentials: true });
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/users/me`)
+        return response.data
     } catch (error) {
-        throw new Error('Error fetching user');
+        throw new Error('Error fetching user')
     }
-};
+}
 
 export const signUp = async (formData: RegisterFormData) => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/users/register`, formData, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/users/register`, formData,)
+        return response.data
     } catch (error) {
-        throw new Error('User already exists');
+        throw new Error('User already exists')
     }
-};
+}
 
 export const signIn = async (formData: SignInFormData) => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/auth/login`, formData, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/auth/login`, formData,)
+        return response.data
     } catch (error) {
-        throw new Error('Invalid credentials');
+        throw new Error('Invalid credentials')
     }
-};
+}
 
 export const validateToken = async () => {
     try {
-        const response = await Axios.get(`${API_BASE_URL}/api/auth/validate-token`, { withCredentials: true });
-        return response.data;
+        const response = await axios.get(`${API_BASE_URL}/api/auth/validate-token`)
+        return response.data
     } catch (error) {
-        throw new Error('Token invalid');
+        throw new Error('Token invalid')
     }
-};
+}
 
 export const signOut = async () => {
     try {
-        const response = await Axios.post(`${API_BASE_URL}/api/auth/logout`, null, { withCredentials: true });
-        return response.data;
+        const response = await axios.post(`${API_BASE_URL}/api/auth/logout`, null)
+        return response.data
     } catch (error) {
-        throw new Error('Error during signout');
+        throw new Error('Error during signout')
     }
-};
+}
