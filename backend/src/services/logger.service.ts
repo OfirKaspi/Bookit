56a3b96b9bfb8180cdecb6cd8fa49da4980c4b16
Ofir Kaspi <kaspiofir@gmail.com>
2024-01-27 15:40:13 +0200

    B/Logger/Logger now logging to the right file

--- backend/src/services/logger.service.ts
+++ backend/src/services/logger.service.ts
@@ -1,46 +1,49 @@
-import fs from 'fs'
+import fs from 'fs';
+import path from 'path';
 
-const logsDir = './logs'
+const logsDir = path.resolve(__dirname, '../logs');
 
 if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir)
+    fs.mkdirSync(logsDir);
 }
+
+const logFilePath = path.join(logsDir, 'backend.log');
 
 export const logger = {
     debug(...args: any[]) {
-        if (process.env.NODE_ENV === 'production') return
-        _doLog('DEBUG', ...args)
+        if (process.env.NODE_ENV === 'production') return;
+        _doLog('DEBUG', ...args);
     },
     info(...args: any[]) {
-        _doLog('INFO', ...args)
+        _doLog('INFO', ...args);
     },
     warn(...args: any[]) {
-        _doLog('WARN', ...args)
+        _doLog('WARN', ...args);
     },
     error(...args: any[]) {
-        _doLog('ERROR', ...args)
-    }
-}
+        _doLog('ERROR', ...args);
+    },
+};
 
 function _getTime() {
-    let now = new Date()
-    return now.toLocaleString('he') // define the time format
+    let now = new Date();
+    return now.toLocaleString('he'); // define the time format
 }
 
 function _isError(e: any): e is Error {
-    return e && e.stack && e.message
+    return e && e.stack && e.message;
 }
 
 function _doLog(level: string, ...args: any[]) {
     const strs = args.map(arg =>
         (typeof arg === 'string' || _isError(arg)) ? arg : JSON.stringify(arg)
-    )
+    );
 
-    const line = `${_getTime()} - ${level} - ${strs.join(' | ')} \n`
+    const line = `${_getTime()} - ${level} - ${strs.join(' | ')} \n`;
 
-    console.log(line)
+    console.log(line);
 
-    fs.appendFile('./logs/backend.log', line, (err) => {
-        if (err) console.log('FATAL: cannot write to log file')
-    })
+    fs.appendFile(logFilePath, line, (err) => {
+        if (err) console.log('FATAL: cannot write to log file');
+    });
 }
